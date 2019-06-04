15a31cba6237b5bcc663d40500ae98ebb9a9e97a
Felipe Wannmacher <fwannmacher@gmail.com>
2019-06-04 21:44:57 +0200

    devkitpro

---
+++ devkitpro/devkitPPC/lib/gcc/powerpc-eabi/8.1.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../../gcc-8.1.0/configure --enable-languages=c,c++,objc,lto --enable-lto --with-cpu=750 --disable-nls --disable-shared --enable-threads=dkp --disable-multilib --disable-win32-registry --disable-libstdcxx-pch --disable-libstdcxx-verbose --enable-libstdcxx-time=yes --enable-libstdcxx-filesystem-ts --enable-cxx-flags='-ffunction-sections -fdata-sections' --target=powerpc-eabi --with-newlib --with-headers=../../newlib-3.0.0/newlib/libc/include --prefix=/opt/devkitpro/devkitPPC --with-system-zlib --with-bugurl=https://github.com/devkitpro/buildscripts/issues --with-pkgversion='devkitPPC release 32' 'CFLAGS_FOR_TARGET=-O2 -ffunction-sections -fdata-sections'";
+static const char thread_model[] = "dkp";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "750" } };
